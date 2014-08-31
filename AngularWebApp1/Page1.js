d05ee05c07056fa9bceda089a7bd34fa1b706c73
Alex Morales <am8850@hotmail.com>
2014-08-31 18:17:33 -0400

    Initial checkin

---
+++ AngularWebApp1/Page1.js
@@ -0,0 +1,72 @@
+﻿(function () {
+
+    var app = angular.module("githubviewer", []);
+
+    var MainController = function ($scope, github, $interval, $log, $anchorScroll, $location) {
+
+
+        // Initialize the array
+        $scope.message = "Github Viewer";
+
+        $scope.repoSortOrder = "-stargazers_count";
+
+        $scope.countdown = 5;
+
+
+        $scope.repos = [];
+
+        $scope.username = "angular";
+
+        var countdownInterval = null;
+
+        // Functions
+
+        var startCountdown = function () {
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        var decrementCountdown = function () {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1)
+                $scope.search($scope.username);
+        };
+
+        $scope.search = function (username) {
+
+            $log.info("Searching for " + username);
+
+            github.getUser(username)
+                 .then(onUserComplete, onError);
+
+            if (countdownInterval) {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+        };
+
+        startCountdown();
+
+        // Event Handlers
+        var onUserComplete = function (data) {
+
+            $scope.user = data;
+
+            github.getRepos(username)
+                 .then(onRepos, onError);
+
+        };
+
+        var onRepos = function (data) {
+            $scope.repos = data;
+            $location.hash("userDetails");
+            $anchorScroll();
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Coult not fetch the data.";
+        };
+
+    };
+
+    app.controller("MainController", ['$scope', 'github', '$interval', '$log', '$anchorScroll', '$location', MainController])
+})();
---
+++ AngularWebApp1/github.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="c:\users\alemor\documents\visual studio 2013\Projects\AngularApps\AngularWebApp1\Scripts/angular.js" />
+(function () {
+
+
+    // create service
+    var github = function ($http) {
+
+        var getUser = function (username) {
+            $http.get("https://api.github.com/users/" + username)
+                 .then(function (response) {
+                     return response.data;
+                 });
+        };
+
+
+        var getRepos = function (username) {
+            $http.get($scope.user.repos_url)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+
+        return {
+            getUser: getUser,
+            getRepos:getRepos
+        };
+    };
+
+    // Get references to module
+    var module = angular.module("githubviewer");
+
+    // Add service to module
+    module.factory("github", github);
+
+})();
